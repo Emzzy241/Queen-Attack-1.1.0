112120ace948fac107d9a6f4897a311f698bfdc1
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-14 12:02:55 +0100

    new updates to code

--- QueenAttack/Program.cs
+++ QueenAttack/Program.cs
@@ -22,60 +22,112 @@ namespace QueenAttack.Models
             //  =bn
                     // Taking X value for other piece
                     Console.WriteLine("Enter the X value for Queen");
-                    int queenValX = int.Parse(Console.ReadLine());
-                    Console.WriteLine();
+                    try
+                    {
+                        int queenValX = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
 
-                    // Taking Y value for other piece
-                    Console.WriteLine("Enter the Y value for Queen");
-                    int queenValY = int.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    Queen newQueen = new Queen(queenValX, queenValY);
-                    
-                    
-                    // Taking X value for other piece
-                    Console.WriteLine();
-                    Console.WriteLine("Enter the X value for any other Chess piece");
-                    int pieceValX = int.Parse(Console.ReadLine());
-                    
-                    // Taking Y value for other piece
-                    Console.WriteLine();
-                    Console.WriteLine("Enter the Y value for any other Chess piece");
-                    int pieceValY = int.Parse(Console.ReadLine());
+                        // Taking Y value for other piece
+                        Console.WriteLine("Enter the Y value for Queen");
+                        int queenValY = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Queen newQueen = new Queen(queenValX, queenValY);
+                        
+                        
+                        // Taking X value for other piece
+                        Console.WriteLine();
+                        Console.WriteLine("Enter the X value for any other Chess piece");
+                        int pieceValX = int.Parse(Console.ReadLine());
+                        
+                        // Taking Y value for other piece
+                        Console.WriteLine();
+                        Console.WriteLine("Enter the Y value for any other Chess piece");
+                        int pieceValY = int.Parse(Console.ReadLine());
 
 
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    bool result = newQueen.CanQueenAttack(pieceValX, pieceValY);
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        
+                        if (pieceValX< 9 && pieceValY < 9 &&  queenValX < 9 &&  queenValY <9)
+                        {
+                            bool result = newQueen.CanQueenAttack(pieceValX, pieceValY);
 
-                    if (result == true)
+                            if (result == true)
+                            {
+                                
+                                Console.WriteLine();
+                                Console.WriteLine(" ---------------------------------------------------------------------");
+                                Console.WriteLine($"Yes, the queen can attack the other chess piece");
+                                Console.WriteLine(" ---------------------------------------------------------------------");
+                                Console.WriteLine();
+                            }
+                            else if (result == false)
+                            {
+                                
+                                Console.WriteLine();
+                                Console.WriteLine(" ---------------------------------------------------------------------");
+                                Console.WriteLine($"No, the queen cannot attack the other chess piece");
+                                Console.WriteLine(" ---------------------------------------------------------------------");
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine();
+                                Console.WriteLine("Something is wrong, ensure you inserted the right coordinates for both Queen and the other chess piece. Lets Try again");
+                                Console.WriteLine();
+                                Console.WriteLine();
+                                Main();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                            Console.WriteLine("Error in values supplied.");
+                            Console.WriteLine("A chess board is divided into 8 * 8; which means any value exceeding 8 will not be accepted");
+                            Console.WriteLine("Please try again");
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Main();
+                        }
+                        
+                        
+
+                    }
+                    catch 
                     {
                         
                         Console.WriteLine();
-                        Console.WriteLine(" ---------------------------------------------------------------------");
-                        Console.WriteLine($"Yes, the queen can attack the other chess piece");
-                        Console.WriteLine(" ---------------------------------------------------------------------");
                         Console.WriteLine();
-                    }
-                    else if (result == false)
-                    {
-                        
-                        Console.WriteLine();
-                        Console.WriteLine(" ---------------------------------------------------------------------");
-                        Console.WriteLine($"No, the queen cannot attack the other chess piece");
-                        Console.WriteLine(" ---------------------------------------------------------------------");
-                        Console.WriteLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine("Something is wrong, ensure you inserted the right coordinates for both Queen and the other chess piece. Lets Try again");
+                        Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        Console.WriteLine("ERROR: Please only enter in numbers! Special symbols and alphabetic characters will not be accepted.");
+                        Console.WriteLine("Please try again...");
                         Console.WriteLine();
                         Console.WriteLine();
                         Main();
                     }
 
+
+
+                    /*catch //(System.Exception ex)
+                    {
+                        // Console.WriteLine("Message = {0}", ex.Message);
+                        // Console.WriteLine("Source = {0}", ex.Source);
+                        // Console.WriteLine("StackTrace = {0}", ex.StackTrace);
+                        // Console.WriteLine("TargetSite = {0}", ex.TargetSite);
+                        // Console.WriteLine("Please enter in numbers only");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        Console.WriteLine("ERROR: Please only enter in numbers! Special symbols and alphabetic characters will not be accepted.");
+                        Console.WriteLine("Please try again...");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Main();
+                    } */
+
+                    
 
                 }
 
